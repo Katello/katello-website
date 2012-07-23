713b6c12d9a95292f08d2d2fdf02ac58ba2f130c
Eric D Helms <ericdhelms@gmail.com>
2012-07-22 23:00:13 -0400

    Adds IRC template page that iframes the Freenode browser client to the Katello channel.

---
+++ php/wp-content/themes/responsive/irc.php
@@ -0,0 +1,30 @@
+<?php
+
+// Exit if accessed directly
+if ( !defined('ABSPATH')) exit;
+
+/**
+ * IRC Page Template
+ *
+   Template Name:  IRC Page
+ *
+ * @file           irc.php
+ * @package        Responsive 
+ * @author         Emil Uzelac 
+ * @copyright      2003 - 2011 ThemeID
+ * @license        license.txt
+ * @version        Release: 1.0
+ * @filesource     wp-content/themes/responsive/irc.php
+ * @link           http://codex.wordpress.org/Theme_Development#Pages_.28page.php.29
+ * @since          available since Release 1.0
+ */
+?>
+<?php get_header(); ?>
+
+        <div id="content-full" class="grid col-940">
+        
+          <iframe src="http://webchat.freenode.net/?channels=#katello" id="irc-iframe" />
+
+        </div><!-- end of #content-full -->
+
+<?php get_footer(); ?>
